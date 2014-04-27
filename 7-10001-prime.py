cdfb8af0938c50b6052517c8717362edab78cf8c
NicholasRBowers <nicholas.ryan.bowers@gmail.com>
2014-04-27 19:30:24 -0400

    Updated 5-8

--- 7-10001-prime.py
+++ 7-10001-prime.py
@@ -5,25 +5,30 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10 001st prime number?
 """
 
+# This code runs in 0.570377111435 seconds.
 import time
 
-def is_prime(n):
-    j = 3
-    while j <= n ** 0.5:
-        if n % j == 0:
-            return False
-        j += 1
-    return True
+# This function determines whether or not an integer, n, is a prime number.
+# Can only analyze positive numbers correctly.
+def is_prime(n):                # Accepts an integer, n, which has its primeness tested.
+    j = 3                       # We start a counter at 3.
+    while j <= n ** 0.5:        # While j is less than or equal to the square root of the number,
+        if n % j == 0:          # If, at any point, n is evenly divisible by j,
+            return False        # Return False.
+        j += 1                  # Increment our counter by 1.
+    return True                 # Return true if we couldn't get the number to divide evenly by any factor.
 
-def prime(n):
-    primes = [2]
-    i = 3
-    while len(primes) < n:
-        if is_prime(i):
-            primes.append(i)
-        i += 2
-    return primes[-1]
+# This function finds the n-th prime.
+def prime(n):                   # Accepts an integer, n, which tells the function which prime to find.
+    primes = [2]                # Start off with a list to hold our primes pre-populated with a 2 (only even prime).
+    i = 3                       # Start the counting at 3.
+    while len(primes) < n:      # While the length of our list is less than the n (n-th prime),
+        if is_prime(i):         # If i is prime,
+            primes.append(i)    # Add it to our list.
+        i += 2                  # Increment by two (all subsequent primes are odd).
+    return primes[-1]           # Return the last member of our list.
 
+#print prime(6)
 s = time.time()
 print prime(10001)
 f = time.time()
