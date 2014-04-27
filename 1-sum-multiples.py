b179ec171c0c19dee6ef047dfe591e644d7f144f
NicholasRBowers <nicholas.ryan.bowers@gmail.com>
2014-04-27 17:50:37 -0400

    Changed file name; added comments to first four solutions; added computation time to first four solutions; fixed an invalid character in fourth solution

---
+++ 1-sum-multiples.py
@@ -0,0 +1,25 @@
+#!/usr/bin/pythonw
+
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+# This code runs in 0.000212907791138 seconds.
+import time
+
+# This function sums the multiples of factors, f, below some limit, n.
+def sum_divisibles(n, *f):          # Accepts many factors as input.
+    sum = 0                         # Variable to keep track of the sum.
+    for i in xrange(1, n):          # Iterate through to the limit.
+        for j in f:                 # Iterate through the factors.
+            if i % j == 0:          # If the current number we're checking is evenly divisible by any of our factors.
+                sum += i            # Add the number to the sum.
+                break               # And break the 'for' loop to avoid double counting when a number is a multiple of more than one of factors.
+    return sum                      # Return the sum.
+
+#print sum_divisibles(10, 3, 5)
+s = time.time()
+print sum_divisibles(1000, 3, 5)
+f = time.time()
+print f - s
--- 3-largest-prime-factor.py
+++ 3-largest-prime-factor.py
@@ -5,13 +5,23 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143?
 """
 
-def largest_prime_factor(n):
-    i = 2
-    while n > i:
-        while n % i == 0:
-            n = n / i
-        i += 1
-    print n
+# This code runs in 0.00055193901062 seconds.
+import time
 
-#largest_prime_factor(13195)
-largest_prime_factor(600851475143)
+# This function finds the largest prime factor of a number, n.
+def largest_prime_factor(n):        # Accepts a number, n, for which the largest prime factor will be found.
+    i = 2                           # Declaring a variable which we'll use to cut n into it's prime factors, we start with 2.
+    while n > i:                    # While our number we're cutting down is bigger than our prime factor,
+        while n % i == 0:           # While our number can be evenly divided by our prime factor,
+            n = n / i               # Cut down n by dividing out the prime factor.
+        if i == 2:                  # If our potential factor is 2,
+            i += 1                  # Increment our potential factor by one.
+        else:                       # Otherwise,
+            i += 2                  # Increment our potential factor by two (all subsequent prime factors are odd).
+    return n                        # We are left with the largest prime factor.  Return this.
+
+#print largest_prime_factor(13195)
+s = time.time()
+print largest_prime_factor(600851475143)
+f = time.time()
+print f - s
